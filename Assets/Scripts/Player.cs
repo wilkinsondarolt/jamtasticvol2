5137b8bafcb4938bb884fab52bc6e52318c059b5
Wilkinson D. de Souza <wilkinson@outlook.com.br>
2017-07-26 01:30:06 -0300

    Character control, camera position and materials

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    Rigidbody rbPlayer;
+    Vector3 velocity;
+    Animator animControl;
+
+    public float moveSpeed = 3;
+    public float smoothMoveTime = 0.1f;
+    public float turnSpeed = 8;
+
+    private void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        animControl = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float inputMagnitude = inputDirection.magnitude;
+        animControl.SetFloat("MoveInput", inputMagnitude);
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+        
+
+        velocity = transform.forward * moveSpeed 
+                                     * smoothInputMagnitude;
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            animControl.SetTrigger("TossCoin");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rbPlayer.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rbPlayer.MovePosition(rbPlayer.position + velocity * Time.fixedDeltaTime);
+        
+    }
+}
---
+++ Assets/Scripts/TopdownCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopdownCamera : MonoBehaviour {
+    public Camera TopDownCamera;
+    public float Distance = 10.0f;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update () {
+        TopDownCamera.transform.position = new Vector3(this.transform.position.x, this.Distance
+                                                                                , this.transform.position.z - 8f);
+	}
+}
