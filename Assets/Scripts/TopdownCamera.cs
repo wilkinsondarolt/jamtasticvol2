4f164a44a080d10476e6c1a644efe8adc9d7c17b
Wilkinson D. de Souza <wilkinson@outlook.com.br>
2017-07-26 23:31:58 -0300

    Ajustes da reunião

--- Assets/Scripts/TopdownCamera.cs
+++ Assets/Scripts/TopdownCamera.cs
@@ -6,11 +6,6 @@ public class TopdownCamera : MonoBehaviour {
     public Camera TopDownCamera;
     public float Distance = 10.0f;
 
-    private void Start()
-    {
-        
-    }
-
     void Update () {
         TopDownCamera.transform.position = new Vector3(this.transform.position.x, this.Distance
                                                                                 , this.transform.position.z - 8f);
