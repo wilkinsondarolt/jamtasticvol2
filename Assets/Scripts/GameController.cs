5e50d42eed414a4f69dbc36a5b14fb48a2c9d6c3
Wilkinson Da Rolt de Souza <wilkinson@ema.corp>
2017-08-01 00:43:39 -0300

    Menu, songs and other fixes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
     public static GameController instance = null;
-
+    public bool Sounds = true;
     void Awake()
     {
         if (instance == null)
